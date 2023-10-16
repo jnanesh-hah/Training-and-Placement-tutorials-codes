1a441fcd9ee4b82cc6313e308426379d0bf35b6d
Jnanesh Kumar KS <101520297+jnanesh-hah@users.noreply.github.com>
2023-10-16 20:32:41 +0530

    Update DAY1.cfullcode

--- DAY1.c
+++ DAY1.c
@@ -3,10 +3,31 @@
 void main(){
 char usrch=' '';
 
-  int count=0;
+  int count=4;
 
   printf("DO you want to signup(s)/Login(1)= ");
-  ipval;
+  ipval:
   scanf("\n %c",&usrch);
-  
+
+  printf("Enter Choice is = %c \n",usrch);
+
+  if(usrch=='l'){
+  printf("Login is Selected\n");
+  }
+  else if(usrch=='s')
+  {
+    printf("Sign up is Selected\n");
+  }
+  else{
+
+    count--;
+    if(count!=0){
+    printf("\nRemaining Count is= %d",count);
+      printf("Worng input , please Enter Valid input =");
+      goto ipval; 
+    }
+    else{
+      printf("You are Exceeded");
+  }
+    printf("Count=%d",count);
 }
