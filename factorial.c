308318e336ceb17753b1f98cfaee637ecd646b63
Jnanesh Kumar KS <101520297+jnanesh-hah@users.noreply.github.com>
2023-10-17 10:25:33 +0530

    Factorial Without recurrsive function

---
+++ factorial.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int fact(int a){
+
+    int res=1;
+    for(int i=a;i>1;i--){
+        res=res*i;
+    }
+    return res;
+    
+}
+   
+
+
+int main(){
+
+    int a=5;
+    int factorial;
+   // printf("Enter the Number to find the factorial = ");
+   // scanf("%d",&a);
+
+    factorial=fact(a);
+    printf("Factorial of %d is %d",a,factorial);
+    return 0;
+
+}
