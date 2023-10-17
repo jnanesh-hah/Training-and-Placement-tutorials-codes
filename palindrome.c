9cfa53f505dbfc12b439f4aaec502f4e47d24885
Jnanesh Kumar KS <101520297+jnanesh-hah@users.noreply.github.com>
2023-10-17 09:41:59 +0530

    Update and rename palindrome.c to palindrome.c updated

--- palindrome.c
+++ palindrome.c
@@ -27,3 +27,33 @@ int main(){
 
 return 0;
 }
+// #include<stdio.h>
+// #include<string.h>
+
+// int main(){
+//     char str1[10]="racecar";
+//     int a=0;
+//     int b=strlen(str1)-1;
+//     int res=0;
+
+//     while(b>a)
+//     {
+//           printf("\nNow We are Comparing %c and %c letters",str1[a],str1[b]);
+//         if(str1[a++]!=str1[b--])
+//         {
+//             printf("\nNow We are Comparing %c and %c letters",str1[a],str1[b]);
+//                 res=1;
+//                 break;
+//         }
+
+//     }
+//     if(res==0){
+//         printf("Palindrome");
+
+//     }
+//     else{
+//         printf(" Not a palindrome");
+//     }
+
+// return 0;
+// }
